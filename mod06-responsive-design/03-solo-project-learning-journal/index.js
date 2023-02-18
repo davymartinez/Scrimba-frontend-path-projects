e043acf96b0e620b4f50279335f8153ed1e2599e
David Martínez <davy.martinez@gmail.com>
2023-02-17 22:56:14 -0300

    Refactored all CSS

--- mod06-responsive-design/03-solo-project-learning-journal/index.js
+++ mod06-responsive-design/03-solo-project-learning-journal/index.js
@@ -3,5 +3,8 @@ const viewMore = document.getElementById("view-more")
 viewMore.addEventListener("click", handleViewMore)
 
 function handleViewMore() {
-    document.getElementById("hidden").classList.toggle("hidden")
+    let toggleText = document.getElementById("hidden")
+    let viewMore = document.getElementById("view-more")
+    toggleText.classList.toggle("hidden")
+    viewMore.innerText = "Toggle View More"
 }
